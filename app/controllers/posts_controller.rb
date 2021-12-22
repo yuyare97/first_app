e2bdf25b1070646782eecf7bfb12f340b3f5e3b4
yuyare97 <yuya.take1020@gmail.com>
2021-12-22 10:59:36 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+
+  def index  # indexアクションを定義した
+    @posts = Post.all  # すべてのレコードを@postsに代入
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+
+end
